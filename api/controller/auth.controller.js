054876035c0173c2795d1227550057bd26fa0677
hasan814 <h.mousavi910@gmail.com>
2024-09-17 21:18:14 +0330

    Create Sign In API route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,8 +1,10 @@
 import { errorHandler } from "../utils/error.js";
 
 import bcrypt from "bcryptjs";
-import User from "../model/user.model.js"; // Assuming you have a user model defined
+import User from "../model/user.model.js";
+import jwt from "jsonwebtoken";
 
+// ================= Sign Up =======================
 export const signup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
@@ -36,5 +38,39 @@ export const signup = async (req, res, next) => {
       .json({ message: "User registered successfully", user: newUser });
   } catch (error) {
     next(errorHandler(500, "Error from the function"));
+  }
+};
+
+// ================= Sign In =======================
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+
+    const user = await User.findOne({ email });
+    if (!user) return next(errorHandler(401, "User not Found!"));
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return next(errorHandler(401, "Invalid email or password"));
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
+    const { password: pass, ...rest } = user._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 };
