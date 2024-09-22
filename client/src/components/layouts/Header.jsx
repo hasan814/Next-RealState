5512ba9ced6898270011780b707e001c88057aa3
hasan814 <h.mousavi910@gmail.com>
2024-09-22 16:03:30 +0330

    Complete Header SearchTerm

--- client/src/components/layouts/Header.jsx
+++ client/src/components/layouts/Header.jsx
@@ -1,10 +1,33 @@
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
 const Header = () => {
+  // ============== Navigate ===============
+  const navigate = useNavigate();
+
+  // ============== State ===============
+  const [searchTerm, setSearchTerm] = useState("");
+
   // ============== Selector ===============
   const { currentUser } = useSelector((state) => state.user);
+
+  // ============== Submit Function ===============
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  // ============== Effect ===============
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFormUrl = urlParams.get("searchTerm");
+    if (searchTermFormUrl) setSearchTerm(searchTermFormUrl);
+  }, []);
 
   // ============== Rendering ===============
   return (
@@ -20,13 +43,20 @@ const Header = () => {
             <span>Real-State</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={submitHandler}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
+            value={searchTerm}
             placeholder="Search..."
+            onChange={(event) => setSearchTerm(event.target.value)}
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4 items-center">
           <Link to={"/"}>
@@ -52,7 +82,7 @@ const Header = () => {
                       p-2
                       list-none
                       rounded-lg 
-                    text-white
+                      text-white
                       duration-300
                     bg-slate-700
                       transition-all 
